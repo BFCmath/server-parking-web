355d6ce6cd528d50fc345d9aa6468e4b3d40e480
Your Name <23120026@student.hcmus.edu.vn>
2023-12-09 20:54:16 +0700

    sua lai port

--- server.js
+++ server.js
@@ -228,5 +228,5 @@ app.post('/login', (req, res) => {
 }); 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port ${PORT}`);
 })
