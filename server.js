d07b1b77a93abb7938c14e616612cbf8d9021d60
Your Name <23120026@student.hcmus.edu.vn>
2023-12-09 23:21:05 +0700

    deploy lan 3

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();  
 app.use(express.json());
 app.use(cors({
-    origin: ["https://bfcmath.github.io/ParkingManagementWeb_user"],
+    origin: [process.env.ORIGIN],
     methods: ["GET", "POST"],
     credentials: true
 }));
